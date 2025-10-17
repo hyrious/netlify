b3964bb6c9fa00f06d158016bcd3eb4af032a5e4
hyrious <hyrious@outlook.com>
2025-10-17 12:01:19 +0800

    update homepage

--- netlify/edge-functions/gonow.ts
+++ netlify/edge-functions/gonow.ts
@@ -1,50 +1,50 @@
-import type { Context } from "@netlify/edge-functions";
+import type { Context } from '@netlify/edge-functions';
 
 export default async (_request: Request, _context: Context) => {
-  const accessKey = Deno.env.get("JSONBIN_GONOW_KEY");
-  if (!accessKey) {
-    return new Response("null", {
-      headers: { "content-type": "application/json" },
-    });
-  }
+	const accessKey = Deno.env.get('JSONBIN_GONOW_KEY');
+	if (!accessKey) {
+		return new Response('null', {
+			headers: { 'content-type': 'application/json' },
+		});
+	}
 
-  const res1 = await fetch(
-    "https://api.jsonbin.io/v3/b/5d1ec090beda8445769e5649",
-    {
-      headers: {
-        "X-Access-Key": accessKey,
-        "X-Bin-Meta": "false",
-      },
-    },
-  );
+	const res1 = await fetch(
+		'https://api.jsonbin.io/v3/b/5d1ec090beda8445769e5649',
+		{
+			headers: {
+				'X-Access-Key': accessKey,
+				'X-Bin-Meta': 'false',
+			},
+		},
+	);
 
-  if (!res1.ok) {
-    return res1;
-  }
+	if (!res1.ok) {
+		return res1;
+	}
 
-  const value = await res1.json().then((e) => e.value);
+	const value = await res1.json().then((e) => e.value);
 
-  const newValue = Number(value) + 1;
+	const newValue = Number(value) + 1;
 
-  const res2 = await fetch(
-    "https://api.jsonbin.io/v3/b/5d1ec090beda8445769e5649",
-    {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-        "X-Access-Key": accessKey,
-      },
-      body: JSON.stringify({ value: newValue }),
-    },
-  );
+	const res2 = await fetch(
+		'https://api.jsonbin.io/v3/b/5d1ec090beda8445769e5649',
+		{
+			method: 'PUT',
+			headers: {
+				'Content-Type': 'application/json',
+				'X-Access-Key': accessKey,
+			},
+			body: JSON.stringify({ value: newValue }),
+		},
+	);
 
-  if (!res2.ok) {
-    return res2;
-  }
+	if (!res2.ok) {
+		return res2;
+	}
 
-  return new Response(String(newValue), {
-    headers: { "Content-Type": "application/json" },
-  });
+	return new Response(String(newValue), {
+		headers: { 'Content-Type': 'application/json' },
+	});
 };
 
-export const config = { path: "/gonow" };
+export const config = { path: '/gonow' };
--- netlify/edge-functions/proxy.ts
+++ netlify/edge-functions/proxy.ts
@@ -1,65 +1,65 @@
-import type { Config, Context } from "@netlify/edge-functions";
+import type { Config, Context } from '@netlify/edge-functions';
 
-export const config: Config = { path: '/proxy/*' }
+export const config: Config = { path: '/proxy/*' };
 
 const Preflight: ResponseInit = {
-  status: 204,
-  headers: new Headers({
-    'access-control-allow-origin': '*',
-    'access-control-allow-methods': 'GET,POST,PUT,PATCH,TRACE,DELETE,HEAD,OPTIONS',
-    'access-control-allow-headers': 'authorization',
-    'access-control-max-age': '1728000',
-  })
-}
+	status: 204,
+	headers: new Headers({
+		'access-control-allow-origin': '*',
+		'access-control-allow-methods': 'GET,POST,PUT,PATCH,TRACE,DELETE,HEAD,OPTIONS',
+		'access-control-allow-headers': 'authorization',
+		'access-control-max-age': '1728000',
+	}),
+};
 
 const NotFound: ResponseInit = {
-  status: 404,
-  headers: new Headers({
-    'access-control-allow-origin': '*',
-  })
-}
+	status: 404,
+	headers: new Headers({
+		'access-control-allow-origin': '*',
+	}),
+};
 
 const whitelist = [
-  'https://registry.npmjs.org/',
-  'https://npm.pkg.github.com/',
-  'https://registry.npmmirror.com/',
-]
+	'https://registry.npmjs.org/',
+	'https://npm.pkg.github.com/',
+	'https://registry.npmmirror.com/',
+];
 
 export default async (request: Request, _context: Context) => {
-  if (request.method === 'OPTIONS' && request.headers.has('access-control-request-headers'))
-    return new Response(null, Preflight)
+	if (request.method === 'OPTIONS' && request.headers.has('access-control-request-headers')) {
+		return new Response(null, Preflight);
+	}
 
-  const url = new URL(request.url)
-  const target_ = url.href.slice(url.origin.length + '/proxy/'.length).replace(/^https?:\/+/, 'https://')
+	const url = new URL(request.url);
+	const target_ = url.href.slice(url.origin.length + '/proxy/'.length).replace(/^https?:\/+/, 'https://');
 
-  if (whitelist.some(prefix => target_.startsWith(prefix))) {
+	if (whitelist.some((prefix) => target_.startsWith(prefix))) {
+		let target: URL;
+		try {
+			target = new URL(target_);
+		} catch {
+			return new Response(null, NotFound);
+		}
 
-    let target: URL
-    try {
-      target = new URL(target_)
-    } catch {
-      return new Response(null, NotFound)
-    }
+		const headers = new Headers(request.headers);
+		headers.set('Host', target.host);
+		headers.set('Referer', target.origin);
 
-    const headers = new Headers(request.headers)
-    headers.set('Host', target.host)
-    headers.set('Referer', target.origin)
+		const response = await fetch(target, {
+			method: request.method,
+			headers: headers,
+			body: request.body,
+		});
 
-    const response = await fetch(target, {
-      method: request.method,
-      headers: headers,
-      body: request.body,
-    })
+		const responseHeaders = new Headers(response.headers);
+		responseHeaders.set('access-control-allow-origin', '*');
+		responseHeaders.set('access-control-allow-credentials', 'true');
+		responseHeaders.delete('content-security-policy');
+		responseHeaders.delete('content-security-policy-report-only');
+		responseHeaders.delete('clear-site-data');
 
-    const responseHeaders = new Headers(response.headers)
-    responseHeaders.set('access-control-allow-origin', '*')
-    responseHeaders.set('access-control-allow-credentials', 'true')
-    responseHeaders.delete('content-security-policy')
-    responseHeaders.delete('content-security-policy-report-only')
-    responseHeaders.delete('clear-site-data')
+		return new Response(response.body, { status: response.status, headers: responseHeaders });
+	}
 
-    return new Response(response.body, { status: response.status, headers: responseHeaders })
-  }
-
-  return new Response(null, NotFound)
-}
+	return new Response(null, NotFound);
+};
